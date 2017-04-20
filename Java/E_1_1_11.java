133bd394db260a58a5837ec4639a8dbb05896d4b
largmost <dingrui.zhen@gmail.com>
2017-04-20 12:11:29 +0800

    some odd change

--- Java/E_1_1_11.java
+++ Java/E_1_1_11.java
@@ -2,20 +2,44 @@ import edu.princeton.cs.algs4.*;
 
 public class E_1_1_11 {
     public static void main(String[] args) {
-        int N = 10;
-        boolean[][] a = new boolean[N][N];
-        a[4][4] = true;
-        for (int j = 0; j < N; j++)
-            StdOut.printf("%2d", j + 1);
+        boolean[][] b = new boolean[StdIn.readInt()][StdIn.readInt()];
+        initial(b);
+        StdOut.print("++");
+        for (int x = 0; x < getColums(b); x++) {
+            StdOut.print("column" + x + " ");
+        }
         StdOut.println();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (a[i][j])
-                    StdOut.print(" *");
-                else
-                    StdOut.print("  ");
+        for (int i = 0; i < b.length; i++) {
+            StdOut.print("line" + i + " ");
+            for (int j = 0; j < b[i].length; j++) {
+                StdOut.print("   " + trans(b[i][j]) + "    ");
             }
             StdOut.println();
         }
+
+    }
+
+    private static boolean[][] initial(boolean[][] b) {
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++)
+                b[i][j] = StdRandom.bernoulli();
+        }
+        return b;
+    }
+
+    private static String trans(boolean b) {
+        if (b)
+            return "*";
+        else
+            return "-";
+    }
+
+    private static int getColums(boolean[][] b) {
+        int columns = 0;
+        for (int i = 0; i < b.length; i++) {
+            if (b[i].length > columns)
+                columns = b[i].length;
+        }
+        return columns;
     }
 }
--- Java/E_1_1_8.java
+++ Java/E_1_1_8.java
@@ -6,7 +6,7 @@ public class E_1_1_8 {
         StdOut.println('b' + 'c');
         StdOut.println('a' + 4);
         // 此处的字符会被隐式转换为整型
-        StdOut.println((char) ("a" + 4));
+        StdOut.println("a" + 4);
         // 此处的字符串会将后面的整型转化为字符串
         StdOut.println((char) ('a' + 4));
     }
